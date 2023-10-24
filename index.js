c1cb8c40c150db2c646e63658ae95f49b894c9fe
IanPol0 <101563167+IanPol0@users.noreply.github.com>
2023-10-24 14:10:38 -0300

    init server

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo!');
-    res.end();
-}).listen(process.env.PORT || 3000);
+const express = require('express');
+const cors = require('cors');
+
+const app = express()
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.json({'hello': 'world'})
+})
+
+app.get('/reset-water', (req, res) => {
+    res.send('Resetting water!')
+})
+
+app.listen(3000, () => {
+    console.log('Listening on port 3000!')
+})
