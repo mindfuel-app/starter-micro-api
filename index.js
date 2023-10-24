c7390a7827ebc4e2b05aec5bc081d47408482848
IanPol0 <101563167+IanPol0@users.noreply.github.com>
2023-10-24 15:51:16 -0300

    jhku

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const {PrismaClient} = require('@prisma/client/edge');
 const { withAccelerate } = require( '@prisma/extension-accelerate')
-const prisma = new PrismaClient().$extends();
+const prisma = new PrismaClient().$extends(withAccelerate());
 const app = express()
 app.use(cors())
 
@@ -10,15 +10,29 @@ app.get('/', (req, res) => {
     res.json({'hello': 'world'})
 })
 
-app.get('/reset-water', (req, res) => {
+app.get('/reset-water', async(req, res) => {
     try{
-        const updatedValues= prisma.water.updateMany({
+        const updatedValues= await prisma.water.updateMany({
             data: {
                 water: 0
             }
         })
         res.json(updatedValues)
     }catch(e){
+        console.log(e)
+    }
+})
+app.get("/users", async (req, res) => {
+    try {
+        const users = await prisma.user.findMany({
+            where: {
+                name: {
+                    contains: 'a'
+                }
+            }
+        })
+        res.json(users)
+    } catch (e) {
         console.log(e)
     }
 })
